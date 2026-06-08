af58834be89b609094a3132b259dcb5850c00fde
Pedro Santos <phcastroestudoss@gmail.com>
2026-06-08 18:41:26 -0300

    Adding User input

---
+++ User input/Program.cs
@@ -0,0 +1,14 @@
+﻿//Here we need the user's interaction to store the values typed in variables.
+//Example:
+Console.Write("What is your name? ");
+String name = Console.ReadLine();
+Console.Write("What is your age? ");
+String age = Console.ReadLine();
+Console.WriteLine($"Hello, {name}. You have {age} years old, correct? Nice to meet you.");
+
+//The code above will return 
+/*
+What is your name? name
+What is your age? age
+Hello, Pedro. You have 25 years old, correct? Nice to meet you.
+*/
