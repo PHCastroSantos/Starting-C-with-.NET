82a2c84b99bc073c0bc3d11917654e63d4928fcf
Pedro Santos <phcastroestudoss@gmail.com>
2026-06-08 17:38:12 -0300

    Starting with C#

---
+++ MyFirstProject/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Qual é o seu nome?");
+String name = Console.ReadLine();
+Console.WriteLine("Qual é a sua idade?");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Muito prazer, " + name + ". Você tem " + age + " anos de idade.") ;
