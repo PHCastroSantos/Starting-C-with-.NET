c38f605810159b6783440d5c36351674f170c134
Pedro Santos <phcastroestudoss@gmail.com>
2026-06-10 16:56:50 -0300

    Adding studies regarding Math class

---
+++ Math Class/Program.cs
@@ -0,0 +1,16 @@
+﻿double a = 30;
+double b = 15;
+
+//System.Console.WriteLine(Math.Pow(a, b));
+
+//System.Console.WriteLine(Math.Sqrt(a));
+
+//System.Console.WriteLine(Math.Abs(b));
+
+//System.Console.WriteLine(Math.Round(a));
+
+//System.Console.WriteLine(Math.Floor(a));
+
+//System.Console.WriteLine(Math.Max(a, b));
+
+System.Console.WriteLine(Math.Min(a, b));
