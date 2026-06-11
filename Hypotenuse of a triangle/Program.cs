f923d1760910692a9b569f83a57cedc0e1df44d9
Pedro Santos <phcastroestudoss@gmail.com>
2026-06-11 16:32:19 -0300

    Adding exercise to calculate Hypotenuse

---
+++ Hypotenuse of a triangle/Program.cs
@@ -0,0 +1,6 @@
+﻿System.Console.Write("Enter side A of the triangle: ");
+double a = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Enter side B of the triangle: ");
+double b = Convert.ToDouble(Console.ReadLine());
+double c = Math.Sqrt((a * a) + (b * b));
+System.Console.Write($"The Hypotenuse is: {c}");
--- Math Class/obj/Debug/net10.0/Math Class.AssemblyInfo.cs
+++ Math Class/obj/Debug/net10.0/Math Class.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Math Class")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c38f605810159b6783440d5c36351674f170c134")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b222c1388679a23d65d4222ff0c396526fd7ccd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Math Class")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Math Class")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
