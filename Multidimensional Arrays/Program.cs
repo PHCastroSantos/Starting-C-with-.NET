b6cd84682181acb6dade247aa74fee48e9ce4874
Pedro Santos <phcastroestudoss@gmail.com>
2026-07-10 16:52:01 -0300

    Adding multidimentional arrays

---
+++ Multidimensional Arrays/Program.cs
@@ -0,0 +1,13 @@
+﻿String [,] parkingLot = { {"Mustang" ,"F-150", "Explorer"},
+                          {"Corvette", "Camaro", "Silverado"},
+                          {"Corolla", "Camry", "Rav4"}
+
+}; 
+
+parkingLot[0, 2] = "Fusion";
+parkingLot[2, 0] = "Ferrari";
+
+foreach(String car in parkingLot)
+{
+    System.Console.WriteLine(car);
+}
