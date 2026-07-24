c4b28f1548e60451a9521a375245595f79c10d4d
Pedro Santos <phcastroestudoss@gmail.com>
2026-07-24 16:37:44 -0300

    Adding array of objects

---
+++ Array of objects/Program.cs
@@ -0,0 +1,23 @@
+﻿Car[] garage = new Car[3];
+
+Car car1 = new Car("Mustang");
+Car car2 = new Car("Corvette");
+Car car3 = new Car("Lamborghini");
+
+garage[0] = car1;
+garage[1] = car2;
+garage[2] = car3;
+
+foreach (Car car in garage)
+{
+ System.Console.WriteLine(car.model);   
+}
+
+class Car
+{
+    public String model;
+    public Car(string model)
+    {
+        this.model = model;
+    }
+}
