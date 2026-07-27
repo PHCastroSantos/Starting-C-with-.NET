95128639f77252b83884561e91702edb789dbee0
Pedro Santos <phcastroestudoss@gmail.com>
2026-07-27 16:22:46 -0300

    Adding methods overriding

---
+++ Method overriding/Program.cs
@@ -0,0 +1,29 @@
+﻿Dog dog = new Dog();
+Cat cat = new Cat();
+
+dog.Speak();
+cat.Speak();
+
+abstract class Animal
+{
+    public virtual void Speak()
+    {
+        System.Console.WriteLine("The animal goes *brrr*");
+    }
+}
+
+class Dog : Animal
+{
+    public override void Speak()
+    {
+        System.Console.WriteLine("wulf wulf, wulf wulf.");
+    }
+}
+
+class Cat : Animal
+{
+    public override void Speak()
+    {
+        System.Console.WriteLine("Meow, meow.");
+    }
+}
