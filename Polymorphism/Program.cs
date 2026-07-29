6a0785b24fc357e021421114dba079c1f5c355f1
Pedro Santos <phcastroestudoss@gmail.com>
2026-07-29 16:12:52 -0300

    Adding Polymorphism and Interfaces

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,42 @@
+﻿Car car = new Car();
+Bycicle bycicle = new Bycicle();
+Boat boat = new Boat();
+
+Vehicle[] vehicles = {car, bycicle, boat};
+
+foreach(Vehicle vehicle in vehicles)
+{
+    vehicle.Go();
+}
+
+class Vehicle
+{
+    public virtual void Go()
+    {
+        
+    }
+}
+
+class Car: Vehicle
+{
+    public override void Go()
+    {
+        System.Console.WriteLine("The car is moving");
+    }
+}
+
+class Bycicle: Vehicle
+{
+        public override void Go()
+    {
+        System.Console.WriteLine("The bycicle is moving");
+    }
+}
+
+class Boat: Vehicle
+{
+        public override void Go()
+    {
+        System.Console.WriteLine("The boat is moving");
+    }
+}
