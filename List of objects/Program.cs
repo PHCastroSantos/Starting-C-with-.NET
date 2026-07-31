c40ec5c778be4409b321b13a7e95a433b5a3f8e0
Pedro Santos <phcastroestudoss@gmail.com>
2026-07-31 16:30:41 -0300

    Adding list of objects

---
+++ List of objects/Program.cs
@@ -0,0 +1,19 @@
+﻿List<Player> players = new List<Player>();
+
+players.Add(new Player("Clark"));
+players.Add(new Player("Garrison"));
+players.Add(new Player("Steve"));
+
+foreach(Player player in players)
+{
+    System.Console.WriteLine(player.username);
+}
+
+class Player
+{
+    public String username;
+    public Player(String username)
+    {
+        this.username = username;
+    }
+}
