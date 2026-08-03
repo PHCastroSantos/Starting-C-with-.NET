29c108bea432a0efdcbaa6d02b3909f756d2d4e8
Pedro Santos <phcastroestudoss@gmail.com>
2026-08-03 16:40:46 -0300

    Adding Auto implemented properties

---
+++ Auto implemented properties/Program.cs
@@ -0,0 +1,11 @@
+﻿Car car = new Car("Porshe");
+System.Console.WriteLine(car.Model);
+class Car
+{
+    public String Model {get; set;}
+
+    public Car(String model)
+    {
+        this.Model = model;
+    }
+}
